fe9cd3d5e79f7616124d612c33ab21679ab35f28
Fritz NAGAE <fritz1414213562@gmail.com>
2025-10-17 02:04:59 +0900

    fix: use startswith in utility.hpp

--- include/parse_indices.hpp
+++ include/parse_indices.hpp
@@ -1,6 +1,6 @@
 #ifndef CONTACTMAP_PARSE_TARGET_INDICES_OPTION
 #define CONTACTMAP_PARSE_TARGET_INDICES_OPTION
-#include<TryAmp/core/core.hpp>
+#include "utility.hpp"
 #include<iostream>
 #include<fstream>
 #include<vector>
@@ -12,7 +12,7 @@ std::vector<int> parse_indices_in(const std::string& fname) {
 	std::vector<int> retval;
 	while (std::getline(ifs, line)) {
 		if (line.empty()) continue;
-		else if (Amp::strmanage::startswith(line, "#")) continue;
+		else if (Utility::startswith(line, "#")) continue;
 		else {
 			retval.push_back(std::stoi(line));
 		}
